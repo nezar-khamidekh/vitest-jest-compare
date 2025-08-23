513b988b1d4154787a20aac14aa7952f4e95762d
Nezar Khamidekh <hamideh@yandex.ru>
2025-08-23 22:10:32 +0300

    languadge pipe types update

--- src/pipes/language/language.pipe.ts
+++ src/pipes/language/language.pipe.ts
@@ -1,13 +1,24 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+interface Translation {
+  [key: string]: string;
+}
+
+interface Translations {
+  [language: string]: Translation;
+}
+
 @Pipe({ name: 'language', standalone: true })
 export class LanguagePipe implements PipeTransform {
   transform(value: string, language: string): string {
-    const translations = {
+    const translations: Translations = {
       en: { greeting: 'Hello' },
       es: { greeting: 'Hola' },
       ru: { greeting: 'Привет' },
     };
-    return translations[language]?.[value] || value;
+
+    const languageTranslations = translations[language];
+
+    return languageTranslations?.[value] || value;
   }
 }
